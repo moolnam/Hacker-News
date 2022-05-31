1e2752a055dbd508fd309a4ec259936d744bc4a3
moolnam <moolnam>
2022-05-31 18:14:22 +0900

    Initial Commit

---
+++ Hacker-News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker-News
+//
+//  Created by KimJongHee on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
